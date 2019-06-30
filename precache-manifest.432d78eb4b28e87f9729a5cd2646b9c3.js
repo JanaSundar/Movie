39cf8f9ae874bae88015139b1f64ae8ba4167106
Janarthanan <janasundar1997@gmail.com>
2019-06-30 16:12:41 +0530

    Updates

---
+++ precache-manifest.432d78eb4b28e87f9729a5cd2646b9c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22d66eca96c776914018aecb06434d0",
+    "url": "/Movie/index.html"
+  },
+  {
+    "revision": "fc34677707569947eef7",
+    "url": "/Movie/static/css/2.cf2bf2bd.chunk.css"
+  },
+  {
+    "revision": "951828089a88b199d15b",
+    "url": "/Movie/static/css/main.0f4f6058.chunk.css"
+  },
+  {
+    "revision": "fc34677707569947eef7",
+    "url": "/Movie/static/js/2.0251d403.chunk.js"
+  },
+  {
+    "revision": "951828089a88b199d15b",
+    "url": "/Movie/static/js/main.f28846a4.chunk.js"
+  },
+  {
+    "revision": "570c646ba74787b03cfc",
+    "url": "/Movie/static/js/runtime~main.780c54cf.js"
+  }
+]);
